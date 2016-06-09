a35816a25139ad2f1860f2fbc9e739ebd22ee8c7
himanthmaddi <maddi.himanth@mtap.in>
2016-06-09 13:59:59 +0530

    first commit

---
+++ gitInit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitInit
+//
+//  Created by Himanth on 09/06/16.
+//  Copyright © 2016 mtap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitInit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitInit
+//
+//  Created by Himanth on 09/06/16.
+//  Copyright © 2016 mtap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
